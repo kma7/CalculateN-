1215457be0ed13805434ed0bdf720fcb7eeeffab
kma7 <makun211@gmail.com>
2015-05-15 15:49:56 -0700

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -2,5 +2,9 @@ public int factorial(int n){
   if(n == 0){
     return 1;
   }
+  if(n * factorial(n - 1) > Integer.MAX_VALUE){
+    System.out.println("n is too large");
+    return 0;
+  }
   return n * factorial(n - 1);
 }
