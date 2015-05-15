88c196504bab45253f90e5ef098f93e68b33be85
kma7 <makun211@gmail.com>
2015-05-15 16:45:22 -0700

    Create test.java

---
+++ test.java
@@ -0,0 +1,12 @@
+
+//Test cases would be:
+/*
+1, Illegal input: -1
+2, lower edge cases: 0, 1
+3, normal input: 5, 6
+4, upper edge cases: 13, 14, 15
+*/
+public void test(){
+  int result = MyClass.factorial(1);
+  assertsEquals(result, 1);
+}
