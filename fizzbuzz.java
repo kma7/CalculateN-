fa38cfcc70561da27e955a50f6c05b6610f20bcb
kma7 <makun211@gmail.com>
2015-05-16 13:18:49 -0700

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,11 +1,16 @@
 public void fizzbuzz(int n){
-  for(int i = 1; i <= n; i++){
-    if(i % 15 == 0){
-      System.out.println("fizzbuzz");
-    }else if(i % 5 == 0){
-      System.out.println("buzz");
-    }else if(i % 3 == 0){
-      System.out.println("fizz");
-    }
-  }
-}
+		if(n < 1){
+			System.out.print("Illegal input.");
+		}
+		for(int i = 1; i <= n; i++){
+			if(i % 15 == 0){
+		      System.out.print("fizzbuzz ");
+		    }else if(i % 5 == 0){
+		      System.out.print("buzz ");
+		    }else if(i % 3 == 0){
+		      System.out.print("fizz ");
+		    }else{
+		    	System.out.print(i + " ");
+		    }
+		}
+	}
